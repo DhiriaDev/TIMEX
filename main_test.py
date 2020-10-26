a4605bf54e081eeec06e018f103f489f67f2ef9f
Alessandro Falcetta <alessandro.falcetta@mail.polimi.it>
2020-10-26 16:18:18 +0100

    Added main_test.py Rename .jsn file to .json

---
+++ main_test.py
@@ -0,0 +1,26 @@
+import timex as tx
+
+# data,stato,ricoverati_con_sintomi,terapia_intensiva,totale_ospedalizzati,isolamento_domiciliare,totale_positivi,variazione_totale_positivi,nuovi_positivi,dimessi_guariti,deceduti,casi_da_sospetto_diagnostico,casi_da_screening,totale_casi,tamponi,casi_testati,note
+
+# PARAMETERS
+param_file_nameJSN = 'configuration_test.json'
+
+# data ingestion
+print('-> INGESTION')
+df, param_config = tx.data_ingestion(param_file_nameJSON)   # ingestion of data
+
+print('-> DESCRIPTION')
+tx.data_description(df, param_config, verbose='yes')
+
+print('-> SELECTION')
+df = tx.data_selection(df, param_config)
+
+print('-> ADD DIFF COLUMN')
+df = tx.add_diff_column(df, df.columns[1], 'incremento_column')
+
+print('-> DESCRIPTION')
+tx.data_description(df, param_config, verbose='yes')
+
+print('-> DESCRIPTION')
+tx.data_frame_visualization(df, param_config, visualization_type="hist", verbose="yes")
+tx.data_frame_visualization(df, param_config, visualization_type="line", mode="stacked", verbose="yes")
