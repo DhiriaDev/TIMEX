b4eb0d50b9355a84573b7bf5bd6a9d6b9ac78f6a
Alessandro Falcetta <alessandro.falcetta@mail.polimi.it>
2022-02-09 15:56:37 +0100

    Fix test for visualization Fix imports (Dash now uses different imports for dcc and html)

--- timexseries/data_visualization/functions.py
+++ timexseries/data_visualization/functions.py
@@ -8,8 +8,8 @@ from pandas import Grouper, DataFrame
 import plotly.graph_objects as go
 import numpy as np
 
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from plotly.subplots import make_subplots
 import networkx as nx
 import dash_bootstrap_components as dbc
