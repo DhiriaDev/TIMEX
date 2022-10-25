b4f73dff4c9101e0dab393a119afee436eae9bc9
FPuoti255 <fpuoti99@gmail.com>
2022-10-25 16:38:22 +0200

    sasl auth adde

--- redpanda_modules/constants.py
+++ redpanda_modules/constants.py
@@ -22,9 +22,12 @@ base_config = {
     "client.id" : "",
     "max.in.flight.requests.per.connection": 5, #(this is the max we can afford. See enable.idempotence in producer config)
     'receive.message.max.bytes' : 2000000000,
-    'security.protocol' : 'ssl',
+    'security.protocol' : 'sasl_ssl',
     # CA certificate file for verifying the broker's certificate.
-    'ssl.ca.location' : './redpanda-ca.crt'
+    'ssl.ca.location' : './redpanda-ca.crt',
+    'sasl.username' : 'dhiria',
+    'sasl.password' : 'piic9xplo8fc',
+    'sasl.mechanisms' : 'SCRAM-SHA-256'
 } 
 
 #########################################################################
