69e18267b101eedfa939c9aa7b51a470c6c7ef77
diegiv <diegoriva1995@gmail.com>
2022-11-16 15:57:32 +0100

    locust test

---
+++ locustfile.py
@@ -0,0 +1,38 @@
+from locust import User, task, between, events
+import string
+import random
+from modules.job_producer import produce_test
+import json
+
+@events.init_command_line_parser.add_listener
+def _(parser):
+    parser.add_argument(
+        'kafka_address',
+        type=str,
+        help='single address (or list of addresses) of the form IP:port[,IP:port]'
+    )
+
+    parser.add_argument(
+        'param_config',
+        type=str,
+        help='Path to the json file from which to load the param config of the job'
+    )
+
+    parser.add_argument(
+        'file_path',
+        type=str,
+        help='Path to were take the input file that is to be sent for the job. The absolute path is suggested'
+    )
+
+class LocustUser(User):
+    wait_time = between(0.1, 5)
+
+    @task
+    def make_test(self):
+        res = ''.join(random.choices(string.ascii_uppercase +
+                                     string.digits, k=10))
+        json_file = open(self.environment.parsed_options.param_config, 'r')
+        param_config = json.load(json_file)
+        json_file.close()
+        param_config["activity_title"] = param_config["activity_title"] + str(res)
+        produce_test(self.environment.parsed_options.kafka_address, param_config, self.environment.parsed_options.file_path)
--- modules/job_producer.py
+++ modules/job_producer.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+
 sys.path.append('./')
 log = logging.getLogger(__name__)
 
@@ -9,26 +10,32 @@ import json
 from Redpanda import JobProducer
 
 
+def produce_test(kafka_address, param_config, file_path):
+    job_producer = JobProducer(client_id=0, kafka_address=kafka_address)
+    job_producer.start_job(param_config, file_path)
+    results = job_producer.end_job()
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument(
-        'kafka_address', 
-        type = str,
+        'kafka_address',
+        type=str,
         help='single address (or list of addresses) of the form IP:port[,IP:port]'
     )
 
     parser.add_argument(
         'param_config',
-        type = str,
+        type=str,
         help='Path to the json file from which to load the param config of the job'
-    ) 
+    )
 
     parser.add_argument(
         'file_path',
-        type = str,
-        help = 'Path to were take the input file that is to be sent for the job. The absolute path is suggested'
+        type=str,
+        help='Path to were take the input file that is to be sent for the job. The absolute path is suggested'
     )
 
     args = parser.parse_args()
@@ -48,21 +55,8 @@ if __name__ == '__main__':
         log.error('a file path has been not specified')
         exit(1)
 
-
     # Opening JSON file
     json_file = open(param_config_path, 'r')
     param_config = json.load(json_file)
     json_file.close()
-
-    job_producer = JobProducer(client_id = 0, kafka_address=kafka_address)
-    job_producer.start_job(param_config, file_path)
-    results = job_producer.end_job()
-    print(results)
-
-
-
-
-
-
-
-
+    produce_test(kafka_address, param_config, file_path)
