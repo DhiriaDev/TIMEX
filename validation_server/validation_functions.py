4b3a6cd2a07726faf0dc50ac4eeda49ac31994e3
diegiv <diegoriva1995@gmail.com>
2022-12-13 16:27:42 +0100

    validation_functions modified to output a formatted json with time-series data and predicition instead of a pickle file

--- validation_server/validation_functions.py
+++ validation_server/validation_functions.py
@@ -1,13 +1,38 @@
-
-from TIMEX.timexseries import TimeSeriesContainer, ValidationPerformance
 import logging
+import pandas as pd
 
 logger = logging.getLogger(__name__)
 
-def validate (timeseries_containers : list[TimeSeriesContainer], param_config : dict) -> TimeSeriesContainer:
-    main_accuracy_estimator = param_config["model_parameters"]["main_accuracy_estimator"]
+# TODO: diamo per scontato che i modelli abbiano tutti lo stesso nome nelle colonne. Scegliere una nomenclatura
+#  standard.
+def filter_prediction_field(best_pred: pd.DataFrame, column: str)-> pd.DataFrame:
+    """
+    Filters out the columns that are not relevant for a "non-debug" result output, when present
+    """
+    if 'yhat_lower' and 'yhat_upper' in best_pred.columns:
+        mux = pd.MultiIndex.from_product([[column], ['yhat', 'yhat_lower', 'yhat_upper']])
+        best_pred = best_pred[['yhat', 'yhat_lower', 'yhat_upper']]
+    elif 'yhat' in best_pred.columns:
+        mux = pd.MultiIndex.from_product([[column], ['yhat']])
+        best_pred = best_pred['yhat']
+    else:
+        logger.info("Unexpected column label in Timeseries_container")
+    df = pd.DataFrame(best_pred.values, columns=mux)
 
-    logger.info ('Validation started using %s as main accuracy estimator!', main_accuracy_estimator)
+    return df
+
+
+def validate(timeseries_containers):
+    main_accuracy_estimator = 'MAE'
+    df_data = pd.DataFrame()
+    prediction = pd.DataFrame()
+    val_err = float
+    json_result = {"data": "",
+                   "best_pred": {}}
+    #models_config = {}
+
+    logger.info('Validation started using %s as main accuracy estimator!', main_accuracy_estimator)
+
     '''
     Since the prediction request are asynchronous for each requested model, now we have to merge the results of
     all the models for the requested timeseries in a single container
@@ -17,16 +42,30 @@ def validate (timeseries_containers : list[TimeSeriesContainer], param_config : 
 
         models = timeseries_container.models
         models_best = {}
+        column = timeseries_container.timeseries_data.columns.values[0]
+        df_data = pd.concat([df_data, timeseries_container.timeseries_data], ignore_index=False, axis=1)
+#        models_config[column] = []
 
         for model in models:
             models[model].results.sort(key=lambda x: getattr(x.testing_performances, main_accuracy_estimator.upper()))
+#            models_config[column].append(models[model].characteristics)
             models_best[model] = getattr(models[model].results[0].testing_performances, main_accuracy_estimator.upper())
 
-        best_model_name = max(models_best, key = models_best.get)
+        val_err = min(models_best.values())
+        best_model = min(models_best, key=models_best.get)
+        best_pred = models[best_model].best_prediction
+        best_pred = filter_prediction_field(best_pred, column)
+        prediction = pd.concat([prediction, best_pred], axis=1)
 
-        timeseries_container.models = { best_model_name : timeseries_container.models[best_model_name] }
-    
+    data_json = df_data.to_json(orient='columns', date_format='iso')
+    prediction = prediction.to_json(orient='columns', date_format='iso')
 
-    logger.info ('Validation finished.')
-    
-    return timeseries_containers
+    json_result["data"] = data_json
+    json_result["best_pred"]["df_pred"] = prediction
+    json_result["best_pred"]["val_err"] = val_err
+#    json_result["models"].append(models_config)
+
+    logger.info('Validation finished.')
+
+    return json_result
+
